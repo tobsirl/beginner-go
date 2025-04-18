27a19f032561f0aa320ed80a2e75d11d903b4d1c
Paul Tobin <tobster111@gmail.com>
2025-04-18 16:19:58 +0100

    refactor: update main function to print variable name instead of static message

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	// This is a simple Go program that prints "Hello, World!" to the console.
-	fmt.Println("Hello, World!")
+	// Variable declaration
+	var name string = "John Doe"
+	fmt.Printf("Hello, %s!\n", name)
 }
