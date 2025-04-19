3a2e899d5d802066bc1c02d17d5f78528d6cfead
Paul Tobin <tobster111@gmail.com>
2025-04-19 09:41:39 +0100

    fix: add switch statement for day of the week in main function

--- main.go
+++ main.go
@@ -62,6 +62,28 @@ func main() {
 	} else {
 		fmt.Println("You are a senior citizen.")
 	}
+
+	day := "Wednesday"
+
+	// switch
+	switch day {
+	case "Monday":
+		fmt.Println("Today is Monday.")
+	case "Tuesday":
+		fmt.Println("Today is Tuesday.")
+	case "Wednesday":
+		fmt.Println("Today is Wednesday.")
+	case "Thursday":
+		fmt.Println("Today is Thursday.")
+	case "Friday":
+		fmt.Println("Today is Friday.")
+	case "Saturday":
+		fmt.Println("Today is Saturday.")
+	case "Sunday":
+		fmt.Println("Today is Sunday.")
+	default:
+		fmt.Println("Invalid day.")
+	}
 }
 
 
